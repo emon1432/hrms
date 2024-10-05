c255681e9d18dc67887366b632e04b19394df532
jony-martin <martinjony38@gmail.com>
2024-10-05 14:35:52 +0600

    registration design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,34 +1,122 @@
+@extends('frontend.layouts.master')
+@section('content')
+    <div class="login-area pt-120 mb-120">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="form-wrapper">
+                        <div class="form-title">
+                            <h3>Log In Here!</h3>
+                            <span></span>
+                        </div>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
 
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="form-inner mb-25">
+                                        <x-label for="email" value="{{ __('Email') }}" />
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+                                        <div class="input-area">
+                                            <img src="{{ asset('frontend') }}/images/icon/email-2.svg" alt />
+                                            <x-input id="email" type="email" name="email" :value="old('email')" required
+                                                autofocus autocomplete="username" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-inner">
+                                        <x-label for="password" value="{{ __('Password') }}" />
+                                        <x-input id="password" type="password" name="password" required
+                                            autocomplete="current-password" />
+                                        <i class="bi bi-eye-slash" id="togglePassword"></i>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-agreement form-inner d-flex justify-content-between flex-wrap">
+                                        <div class="form-group">
+                                            <input type="checkbox" id="html" />
+                                            <label for="html" class="flex items-center">
+                                                <x-checkbox id="html" name="remember" />
+                                                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                                            </label>
+                                        </div>
+                                        @if (Route::has('password.request'))
+                                            <a class="forgot-pass" href="{{ route('password.request') }}">
+                                                {{ __('Forgot your password?') }}
+                                            </a>
+                                        @endif
 
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                                        {{-- <a href="#" class="forgot-pass">Forget Password?</a> --}}
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-inner">
+                                        <x-button class="primry-btn-2">
+                                            {{ __('Log in') }}
+                                        </x-button>
+
+                                        {{-- <button class="primry-btn-2" type="submit">LogIn</button> --}}
+                                    </div>
+                                </div>
+                                <h6>
+                                    Don’t have an account? <a href="{{ route('register') }}">Sign Up</a>
+                                </h6>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
+        </div>
+    </div>
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+    {{-- <div class="login-area pt-120 mb-120">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="form-wrapper">
+                        <div class="form-title">
+                            <h3>Log In Here!</h3>
+                            <span></span>
+                        </div>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <div class="form-inner mb-25">
+                                <x-label for="email" value="{{ __('Email') }}" />
+                                <x-input id="email" class="block mt-1 w-full" type="email" name="email"
+                                    :value="old('email')" required autofocus autocomplete="username" />
+                            </div>
+
+                            <div class="form-inner mt-4">
+                                <x-label for="password" value="{{ __('Password') }}" />
+                                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                                    autocomplete="current-password" />
+                            </div>
+
+                            <div class="form-group block mt-4">
+                                <label for="remember_me" class="flex items-center">
+                                    <x-checkbox id="remember_me" name="remember" />
+                                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                                </label>
+                            </div>
+
+                            <div class="flex items-center justify-end mt-4">
+                                @if (Route::has('password.request'))
+                                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                        href="{{ route('password.request') }}">
+                                        {{ __('Forgot your password?') }}
+                                    </a>
+                                @endif
+
+                                <x-button class="form-inner ms-4">
+                                    {{ __('Log in') }}
+                                </x-button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
+        </div>
+    </div> --}}
+@endsection
