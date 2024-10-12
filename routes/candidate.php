72c16f4ff15414b4b33d7dc33b32c45bd4702651
emon1432 <ovirajemon143@gmail.com>
2024-10-13 02:38:03 +0600

    Add CandidateController and route for candidate dashboard

--- routes/candidate.php
+++ routes/candidate.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\Frontend\CandidateController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified', 'candidate'])->group(function () {
-    Route::get('/my-dashboard', function () {
-        return view('backend.pages.dashboard.index');
-    })->name('candidate.dashboard');
+    Route::controller(CandidateController::class)->group(function () {
+        Route::get('/my-dashboard', 'dashboard')->name('candidate.dashboard');
+    });
 });
